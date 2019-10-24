1e5bd82b747b29935a8742256259904a79ba7e19
HSURu <55227731+HSURu@users.noreply.github.com>
2019-10-24 14:51:46 +0800

    20191024

---
+++ UI/Assets/Script/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;  //引用 音頻 API
+
+public class GameManager : MonoBehaviour
+{
+    // 定義欄位 (宣告變數)
+    //修飾詞 類型 名稱 結尾
+    //public 公開、private 私人
+    public AudioMixer mixer;
+
+
+}
