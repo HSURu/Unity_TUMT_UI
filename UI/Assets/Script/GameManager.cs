d8b051dec07dc690a36491e22f86125976a7e9f0
HSURu <55227731+HSURu@users.noreply.github.com>
2019-10-31 14:47:40 +0800

    20191031

--- UI/Assets/Script/GameManager.cs
+++ UI/Assets/Script/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.Audio;  //引用 音頻 API
+using UnityEngine.Audio;    //引用 音頻 API
+using UnityEngine.UI;       //引用 介面 API
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +9,24 @@ public class GameManager : MonoBehaviour
     //修飾詞 類型 名稱 結尾
     //public 公開、private 私人
     public AudioMixer mixer;
+    public Text loadingText;    //文字
+    public Slider loading;      //滑桿
 
+    //定義方法(宣告函式)
+    //修飾詞 類型 名稱 (參數) {敘述}
+    public void SetVBGM(float value)
+    {
+        //音效管理器.設定浮點數("名稱" ，值);
+        mixer.SetFloat("VBGM", value);
+    }
 
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("場景");
+    }
 }
