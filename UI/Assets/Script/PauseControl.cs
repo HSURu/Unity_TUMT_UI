de3d4259e90305f72e096d6ae3db0d15819a52f6
HSURu <55227731+HSURu@users.noreply.github.com>
2019-12-12 14:54:57 +0800

    1212

---
+++ UI/Assets/Script/PauseControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imaagePause;
+    public Sprite spritePause, spritePlay;
+
+    public bool pause;
+ 
+}
